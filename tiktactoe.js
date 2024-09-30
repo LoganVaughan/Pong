9ce672b84e9ecc99ff16fd07412363cc901476c9
Logan Vaughan <LBVaughan25@gmail.com>
2024-09-30 10:27:20 -0400

    Testing

---
+++ tiktactoe.js
@@ -0,0 +1,13 @@
+// How to play tik tak toe
+
+// Create a game board
+
+// Assign shapes to players
+
+// Deterine which player goes first and which goes second
+
+//Loop through these until the board is full or someone wins
+// Player 1 puts an O in a empty game box
+// Player 2 puts a X in another empty game box
+
+// Displat the results - player won or was a draw
